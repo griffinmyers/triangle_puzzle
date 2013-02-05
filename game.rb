b99d8b57ad645e014cceb72a71450c912b86a2f8
William Myers <griffin.myers@gmail.com>
2013-02-05 12:04:05 -0500

    Major performance tweaks

--- game.rb
+++ game.rb
@@ -1,21 +1,21 @@
 require_relative 'board'
 require_relative 'node'
 
+# Start with the 0 peg being open
 board = Board.new(0)
 
 def play(board)
-
   if board.moves.any?
     board.moves.each do |move|
-      board_dup = Marshal.load(Marshal.dump(board))
-      board_dup.move(move[0], move[1], move[2])
-      play(board_dup)
+      board.move(move[0], move[1], move[2])
+      play(board)
+      board.rewind(move[0], move[1], move[2])
     end
   else
     if board.remaining == 1
-      puts " ##begin solution ##"
+      puts " ## begin solution ##"
       puts board.history
-      puts " ##end solution ##"
+      puts " ## end solution ##"
     end
   end
 
