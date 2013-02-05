d9aacf4b716013f2437f43b1a4209ae3f9a4ae0e
William Myers <griffin.myers@gmail.com>
2013-02-05 12:00:21 -0500

    Major performance tweaks

--- board.rb
+++ board.rb
@@ -5,13 +5,30 @@ class Board
       Node.new(n, n != open_pos, self)
     end
     @history = []
+    @activenodes = (0..14).to_a.delete_if { |n| n == open_pos }
+    @remaining = 14
   end
 
   def move(from, to, over)
     @nodes[from].isOccupied = false
     @nodes[to].isOccupied = true
     @nodes[over].isOccupied = false
+    @activenodes.delete_if { |n| n == from }
+    @activenodes.delete_if { |n| n == over }
+    @activenodes.push(to)
     @history.push("from: #{from}, to: #{to}, over: #{over}")
+    remaining -= 1
+  end
+
+  def rewind(from, to, over)
+    @nodes[from].isOccupied = true
+    @nodes[to].isOccupied = false
+    @nodes[over].isOccupied = true
+    @activenodes.delete_if { |n| n == to }
+    @activenodes.push(from)
+    @activenodes.push(over)
+    @history.pop
+    remaining += 1
   end
 
   def nodes
@@ -19,16 +36,16 @@ class Board
   end
 
   def remaining
-    @nodes.reduce(0) do |memo, node|
-      memo = memo + 1 if node.isOccupied
-      memo
-    end
+    @remaining
   end
 
   def moves
-    node = @nodes.reduce([]) do |memo, node|
-      memo + node.moves
+    m = []
+    @activenodes.each do |node_num|
+      moves = @nodes[node_num].moves
+      m += moves if moves.any?
     end
+    m
   end
 
   def history
